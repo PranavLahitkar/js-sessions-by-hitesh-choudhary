55baab465261ecc3de9b7e0c7a17ac171fa2ba3d
Pranav Arun Lahitkar <109038291+PranavLahitkar@users.noreply.github.com>
2024-03-21 23:54:29 +0530

    07_datesinJs.js

--- basics_1/07_datesinJs.js
+++ basics_1/07_datesinJs.js
@@ -1,27 +1,27 @@
-// Dates
+// experimenting Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+let myCreatedDate = new Date(2023, 0, 23)
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("06-08-2001")
+console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
 console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+console.log(newDate.getMonth() + 1);//current month number +1
+console.log(newDate.getDay());//get day on the week
 
 // `${newDate.getDay()} and the time `
 
