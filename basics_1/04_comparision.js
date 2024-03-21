54d3db0559a594644f60f4daa8a62754b514506d
Pranav Arun Lahitkar <109038291+PranavLahitkar@users.noreply.github.com>
2024-03-21 23:45:14 +0530

    Update 04_comparision.js

--- basics_1/04_comparision.js
+++ basics_1/04_comparision.js
@@ -1,21 +1,23 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+ console.log(2 > 1);
+ console.log(2 >= 1);
+ console.log(2 < 1);
+ console.log(2 == 1);
+ console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); //false
+console.log(null == 0);//flase
+console.log(null >= 0);//true  
+//he equality check == for undefined and null is defined such that, 
+  //without any conversions, they equal each other and don't equal anything else. That's why (2) null == 0 is false.
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0); //false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
 
-// === 
+// === its like 
 
-console.log("2" === 2);
+console.log("2" === 2); //strict equality op will be false
